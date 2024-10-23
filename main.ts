b7b0bd4bb81fc3099a31a37a31f5f0249a6d507b
bain-liao <liao.bain@mths.ca>
2024-10-23 11:14:24 -0400

    created program to move a servo

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Bain Liao
+ * Created on: Oct 2024
+ * This program moves a servo
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+//rotates servo 69 degrees
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoNumber1, 69)
+    basic.clearScreen()
+    basic.showString('69')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+//rotates servo 138 degrees
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 138)
+    basic.clearScreen()
+    basic.showString('138')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
